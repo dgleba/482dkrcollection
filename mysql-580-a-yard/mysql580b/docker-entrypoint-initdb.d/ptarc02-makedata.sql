d9c16a0169953f01971d1a32a1b930d788fefffd
David Gleba <dgleba@gmail.com>
2021-04-14 22:49:35 -0400

    2021-04-14c

---
+++ mysql-580-a-yard/mysql580b/docker-entrypoint-initdb.d/ptarc02-makedata.sql
@@ -0,0 +1,33 @@
+
+drop table if exists mydates;
+drop table if exists mydates_archive;
+
+CREATE TABLE mydates_archive
+(
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  mydate datetime,
+  PRIMARY KEY (`id`)
+);
+CREATE TABLE mydates
+(
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  mydate datetime,
+  PRIMARY KEY (`id`)
+);
+
+DROP PROCEDURE IF EXISTS filldates;
+DELIMITER ||
+CREATE PROCEDURE filldates(dateStart DATETIME, dateEnd DATETIME)
+BEGIN
+  WHILE dateStart <= dateEnd DO
+    INSERT INTO mydates (mydate) VALUES (dateStart);
+    SET dateStart = date_add(dateStart, INTERVAL 1 minute);
+  END WHILE;
+END;
+||
+DELIMITER ;
+CALL filldates('2021-04-14 16:00:00','2021-04-14 21:00:00');
+
+select * from mydates limit 2;
+
+
